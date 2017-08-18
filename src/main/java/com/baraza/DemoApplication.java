a1b307fa6ed4a70cf46e40f20410273e376470fe
Obadha <wobadha@gmail.com>
2017-08-18 15:12:00 +0300

    Enter Java Spring

---
+++ src/main/java/com/baraza/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.baraza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home() {
+    return "Hello! This is a demo application.";
+
+  }
+
+  @RequestMapping("/hello")
+  @ResponseBody
+  String hello() {
+    return "Hello from Heroku!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
---
+++ src/main/java/com/baraza/User.java
@@ -0,0 +1,32 @@
+package com.baraza;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @NotEmpty
+  private String firstName;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+}
